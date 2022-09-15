de647bc5e3f536d683ac28e680fe0e3b030c1d43
nakashima <nakashima@crasco.jp>
2022-09-15 11:28:49 +0900

    エンコーディング別に適切な区切りで変換できるように修正(修正前は文字化けしてた)

--- src/Goodby/CSV/Import/Standard/StreamFilter/ConvertMbstringEncoding.php
+++ src/Goodby/CSV/Import/Standard/StreamFilter/ConvertMbstringEncoding.php
@@ -28,12 +28,17 @@ class ConvertMbstringEncoding extends php_user_filter
     private $toCharset;
 
     /**
+     * @var string
+     */
+    private $buffer = '';
+
+    /**
      * Return filter name
      * @return string
      */
     public static function getFilterName()
     {
-        return self::FILTER_NAMESPACE.'*';
+        return self::FILTER_NAMESPACE . '*';
     }
 
     /**
@@ -42,12 +47,12 @@ class ConvertMbstringEncoding extends php_user_filter
      */
     public static function register()
     {
-        if ( self::$hasBeenRegistered === true ) {
+        if (self::$hasBeenRegistered === true) {
             return;
         }
 
-        if ( stream_filter_register(self::getFilterName(), __CLASS__) === false ) {
-            throw new RuntimeException('Failed to register stream filter: '.self::getFilterName());
+        if (stream_filter_register(self::getFilterName(), __CLASS__) === false) {
+            throw new RuntimeException('Failed to register stream filter: ' . self::getFilterName());
         }
 
         self::$hasBeenRegistered = true;
@@ -62,7 +67,7 @@ class ConvertMbstringEncoding extends php_user_filter
      */
     public static function getFilterURL($filename, $fromCharset, $toCharset = null)
     {
-        if ( $toCharset === null ) {
+        if ($toCharset === null) {
             return sprintf('php://filter/convert.mbstring.encoding.%s/resource=%s', $fromCharset, $filename);
         } else {
             return sprintf('php://filter/convert.mbstring.encoding.%s:%s/resource=%s', $fromCharset, $toCharset, $filename);
@@ -74,13 +79,13 @@ class ConvertMbstringEncoding extends php_user_filter
      */
     public function onCreate()
     {
-        if ( strpos($this->filtername, self::FILTER_NAMESPACE) !== 0 ) {
+        if (strpos($this->filtername, self::FILTER_NAMESPACE) !== 0) {
             return false;
         }
 
         $parameterString = substr($this->filtername, strlen(self::FILTER_NAMESPACE));
 
-        if ( ! preg_match('/^(?P<from>[-\w]+)(:(?P<to>[-\w]+))?$/', $parameterString, $matches) ) {
+        if (!preg_match('/^(?P<from>[-\w]+)(:(?P<to>[-\w]+))?$/', $parameterString, $matches)) {
             return false;
         }
 
@@ -90,6 +95,9 @@ class ConvertMbstringEncoding extends php_user_filter
         return true;
     }
 
+    // NOTE: 参考URL
+    // https://qiita.com/qwe001/items/fab183cf94f6aac7df00
+    // https://qiita.com/rana_kualu/items/dc99be34b9e2f721b70f#comment-2359b95663c743e1a776
     /**
      * @param string $in
      * @param string $out
@@ -99,12 +107,33 @@ class ConvertMbstringEncoding extends php_user_filter
      */
     public function filter($in, $out, &$consumed, $closing)
     {
-        while ( $bucket = stream_bucket_make_writeable($in) ) {
-            $bucket->data = mb_convert_encoding($bucket->data, $this->toCharset, $this->fromCharset);
+        while ($bucket = stream_bucket_make_writeable($in)) {
+            $fullData = $this->buffer . $bucket->data;
             $consumed += $bucket->datalen;
-            stream_bucket_append($out, $bucket);
+            if ($this->isValidEncoding($fullData)) {
+                $bucket->data = $this->encode($fullData);
+                $this->clearBuffer();
+                stream_bucket_append($out, $bucket);
+            } else {
+                $this->buffer = $fullData;
+                return PSFS_FEED_ME;
+            }
         }
-
         return PSFS_PASS_ON;
+    }
+
+    private function isValidEncoding(string $string): bool
+    {
+        return mb_check_encoding($string, $this->fromCharset);
+    }
+
+    private function encode(string $string): string
+    {
+        return mb_convert_encoding($string, $this->toCharset, $this->fromCharset);
+    }
+
+    private function clearBuffer(): void
+    {
+        $this->buffer = '';
     }
 }
